dccf6bd9c79fcec1b794f583ea451795f2c784b5
agomesd <alex.gduguay@gmail.com>
2026-04-16 10:05:38 +0100

    Feeds_follow migrations

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/xml"
 	"fmt"
+	"html"
+	"io"
+	"net/http"
 	"os"
 	"time"
 
@@ -27,12 +31,41 @@ type commands struct {
 	cmds map[string]func(s *state, cmd command) error
 }
 
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDat      string `xml:"pubDate"`
+}
+
 func (c *commands) run(s *state, cmd command) error {
 	return c.cmds[cmd.name](s, cmd)
 }
 
 func (c *commands) register(name string, f func(*state, command) error) {
 	c.cmds[name] = f
+}
+
+func handlerFeeds(s *state, cmd command) error {
+	feeds, err := s.db.GetFeeds(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for _, feed := range feeds {
+		fmt.Printf("Feed: %s - From: %s - For user: %s\n", feed.Name, feed.Url, feed.Username.String)
+	}
+
+	return nil
 }
 
 func handlerReset(s *state, cmd command) error {
@@ -87,6 +120,97 @@ func handlerLogin(s *state, cmd command) error {
 	return nil
 }
 
+func handlerUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for _, user := range users {
+		isCurrent := s.config.CurrentUserName == user.Name
+		if isCurrent {
+			fmt.Printf("* %s (current)\n", user.Name)
+		} else {
+			fmt.Printf("* %s\n", user.Name)
+		}
+	}
+
+	return nil
+}
+
+func agg(s *state, cmd command) error {
+	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(feed)
+	return nil
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	feed := &RSSFeed{}
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return feed, err
+	}
+
+	req.Header.Set("User-Agent", "gator")
+
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return feed, err
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return feed, err
+	}
+	defer res.Body.Close()
+
+	if err = xml.Unmarshal(data, feed); err != nil {
+		return feed, err
+	}
+
+	cleaned := cleanFeed(*feed)
+
+	return &cleaned, nil
+
+}
+
+func addFeed(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("feed name and url is required")
+	}
+
+	if len(cmd.args) == 1 {
+		return fmt.Errorf("feed url is required")
+	}
+
+	user, err := s.db.GetUser(context.Background(), s.config.CurrentUserName)
+	if err != nil {
+		return err
+	}
+
+	params := database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[0],
+		Url:       cmd.args[1],
+		UserID:    user.ID,
+	}
+
+	feed, err := s.db.CreateFeed(context.Background(), params)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(feed)
+	return nil
+}
+
 func main() {
 	c, err := config.Read()
 
@@ -105,6 +229,10 @@ func main() {
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerUsers)
+	cmds.register("agg", agg)
+	cmds.register("addfeed", addFeed)
+	cmds.register("feeds", handlerFeeds)
 
 	args := os.Args
 
@@ -135,4 +263,21 @@ func main() {
 	}
 
 	os.Exit(0)
+}
+
+func cleanFeed(f RSSFeed) RSSFeed {
+	f.Channel.Title = html.UnescapeString(f.Channel.Title)
+	f.Channel.Description = html.UnescapeString(f.Channel.Description)
+	newItems := []RSSItem{}
+	for _, item := range f.Channel.Item {
+		cleanedItem := RSSItem{
+			Title:       html.UnescapeString(item.Title),
+			Description: html.UnescapeString(item.Description),
+			Link:        item.Link,
+			PubDat:      item.PubDat,
+		}
+		newItems = append(newItems, cleanedItem)
+	}
+	f.Channel.Item = newItems
+	return f
 }
---
+++ sql/queries/feeds.sql
@@ -0,0 +1,22 @@
+-- name: CreateFeed :one
+INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
+VALUES ($1, $2, $3, $4, $5, $6)
+RETURNING *;
+-- name: GetFeeds :many
+SELECT feeds.name,
+    feeds.url,
+    users.name AS username
+FROM feeds
+    LEFT JOIN users ON users.id = feeds.user_id;
+-- name: CreateFeedFollow :one
+WITH inserted_feed_follow AS (
+    INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *
+)
+SELECT inserted_feed_follow.*,
+    feeds.name AS feed_name,
+    users.name AS user_name
+FROM inserted_feed_follow
+    INNER JOIN feeds ON feeds.id = inserted_feed_follow.feed_id
+    INNER JOIN users ON users.id = inserted_feed_follow.user_id;
